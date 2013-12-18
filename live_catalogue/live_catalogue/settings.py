ce6754b3c350b879cce4ec1bcf6e6d0718ad598d
catardra <catardra@60810c31-268d-49e0-9aa7-0fbff4fae3ae>
2013-12-18 12:18:41 +0000

    change MEDIA_ROOT
    git-svn-id: https://svn.eionet.europa.eu/repositories/Python/flis.live_catalogue@244 60810c31-268d-49e0-9aa7-0fbff4fae3ae

--- live_catalogue/live_catalogue/settings.py
+++ live_catalogue/live_catalogue/settings.py
@@ -116,7 +116,7 @@ MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'instance')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 FRAME_COOKIES = ['__ac', '_ZopeId']
 
