e32d68fad556292cff92a1912e00c2889a3f80c8
catardra <catardra@60810c31-268d-49e0-9aa7-0fbff4fae3ae>
2014-01-07 15:41:09 +0000

    modifications to notifications
    git-svn-id: https://svn.eionet.europa.eu/repositories/Python/flis.live_catalogue@254 60810c31-268d-49e0-9aa7-0fbff4fae3ae

--- live_catalogue/notifications/definitions.py
+++ live_catalogue/notifications/definitions.py
@@ -4,12 +4,12 @@ SCHEMA = 'http://projects.eionet.europa.eu/flis-services-project/' \
 
 
 RDF_URI = {
-    'rdf_type': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type',
     'title': 'http://purl.org/dc/elements/1.1/title',
     'identifier': 'http://purl.org/dc/elements/1.1/identifier',
     'date': 'http://purl.org/dc/elements/1.1/date',
     'catalogue_event': SCHEMA + '#catalogue_event',
+    'catalogue_kind': SCHEMA + '#catalogue_kind',
+    'event_type': SCHEMA + '#event_type',
     'actor': SCHEMA + '#actor',
     'actor_name': SCHEMA + '#actor_name',
-    'event_type': SCHEMA + '#event_type',
 }
--- live_catalogue/notifications/views.py
+++ live_catalogue/notifications/views.py
@@ -23,11 +23,11 @@ def subscribe(user_id):
 
 
 def prepare_notification_rdf(item, event_type):
-    title = item.subject
+    title, kind = item.subject, item.kind_verbose
+    actor = item.user_id
     url = reverse('catalogue_view', kwargs={'pk': item.pk, 'kind': item.kind})
     identifier = settings.HOSTNAME + url
     date = localtime(item.created_on).strftime('Y-%b-%d %H:%M:%S')
-    actor = item.user_id
     event_data = [
         (RDF_URI['rdf_type'], RDF_URI['catalogue_event']),
         (RDF_URI['title'], title),
@@ -35,6 +35,7 @@ def prepare_notification_rdf(item, event_type):
         (RDF_URI['date'], date),
         (RDF_URI['actor'], actor),
         (RDF_URI['actor_name'], actor),
+        (RDF_URI['catalogue_kind'], kind),
         (RDF_URI['event_type'], event_type),
     ]
     return [[url, pred, obj] for pred, obj in event_data]
@@ -46,12 +47,12 @@ def notify(item, event_type):
 
 def send_notification(rdf_triples):
     channel_id = settings.UNS_CHANNEL_ID
-    send_notifications = getattr(settings, 'UNS_SUPPRESS_NOTIFICATIONS', False)
-    if send_notifications:
+    surpress = getattr(settings, 'UNS_SUPPRESS_NOTIFICATIONS', False)
+    if not surpress:
         # log.info("Notification via UNS for %s", rdf_triples[0][0])
         # log.debug("Notification data: %r", rdf_triples)
         uns = get_uns_proxy()
-        # uns.sendNotification(channel_id, rdf_triples)
+        uns.sendNotification(channel_id, rdf_triples)
     else:
         # log.info("Notification via UNS for %s (not sent)", rdf_triples[0][0])
         pass
